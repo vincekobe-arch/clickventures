c99737c1756fe59c5d82cf63e2f56803ce8fdd48
Vince Kobe <cvincekobe@gmail.com>
2026-05-28 19:36:43 +0800

    fix: update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,7 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost/clickventures-api' });
+
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost/clickventures-api'
+});
+
 export default API;
